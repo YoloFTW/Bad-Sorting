ef6521bea4d7f77f14a40bb339712c9bfcecc381
Andre <andretoddriddle@hotmail.com>
2024-05-09 02:22:06 +0100

    added intelligent sort

---
+++ utilities/sorting/IntelligentSort.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    /**
+     * Intelligent Sorts the array
+     * @param {Array} arr array to sort
+     * @returns {Array} 
+     */
+    IntelligentSort: (arr) => {
+
+        return arr;
+
+    }
+
+}
