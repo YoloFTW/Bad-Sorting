432aeff05144e13eb63e820bf6eba5bbf9acd586
Andre <andretoddriddle@hotmail.com>
2024-05-09 02:22:28 +0100

    added quantum bogo sort

---
+++ utilities/sorting/QuantumBogoSort.js
@@ -0,0 +1,23 @@
+const { IsSorted } = require("../IsSorted")
+
+module.exports = {
+
+    /**
+     * Quantum Bogo Sorts the array
+     * @param {Array} arr array to sort
+     * @returns {Array} 
+     */
+    QuantumBogoSort: (arr) => {
+
+        if (IsSorted(arr)) {
+            return arr;
+        
+        }else{
+
+            //TODO: Impliment destruction of universe
+
+        }
+
+    }
+
+}
