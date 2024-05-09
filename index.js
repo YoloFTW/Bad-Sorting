dc97c024983c1db6c543129c6233756276b6c27a
Andre <andretoddriddle@hotmail.com>
2024-05-09 02:23:35 +0100

    exported sorting algorithims

---
+++ index.js
@@ -0,0 +1,16 @@
+const { BogoSort } = require('./utilities/sorting/BogoSort');
+const { CommunismSort } = require('./utilities/sorting/CommunismSort');
+const { IntelligentSort } = require('./utilities/sorting/IntelligentSort');
+const { MiracleSort } = require('./utilities/sorting/MiracleSort');
+const { QuantumBogoSort } = require('./utilities/sorting/QuantumBogoSort');
+const { SleepSort } = require('./utilities/sorting/SleepSort');
+
+module.exports.BogoSort = BogoSort;
+module.exports.CommunismSort = CommunismSort;
+module.exports.IntelligentSort = IntelligentSort;
+module.exports.MiracleSort = MiracleSort;
+module.exports.QuantumBogoSort = QuantumBogoSort;
+module.exports.SleepSort = SleepSort;
+
+
+
